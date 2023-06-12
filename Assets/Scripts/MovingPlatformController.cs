cb46e9df4204fca23b1aeb21100fea093abafa7b
ShoumoPal <arka.pal21@gmail.com>
2023-06-12 19:14:58 +0530

    Added final touches( Little more to add )

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private bool isMoving;
+    [SerializeField] private float speed;
+    private Transform currentTarget;
+    
+    private void Start()
+    {
+        transform.position = pointA.position;
+        currentTarget = pointB;
+        isMoving = true;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            if (Vector2.Distance(transform.position, currentTarget.position) < 0.02f)
+            {
+                isMoving = false;
+                Invoke("ChangeDirection", 2f);
+            }
+            transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+        }
+    }
+    private void ChangeDirection()
+    {
+        if(currentTarget == pointB)
+        {
+            currentTarget = pointA;
+        }
+        else
+        {
+            currentTarget = pointB;
+        }
+        isMoving = true;
+    }
+}
